6585c9338eafe54811e560d18bd1f63254aa5f94
EC-creator67 <ettorecox@gmail.com>
2026-09-20 09:16:56 -0400

    changes in Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
         className="absolute flex flex-col items-start gap-2 max-w-[50%] 
         bottom-[25%] left-6 "
       >
-        <h2 className="text-xl md:text-3xl  lg:text-4xl font-semibold  text-violet-800 fade-in">
+        <h2 className="text-xl md:text-3xl italic lg:text-4xl font-semibold  text-violet-800 fade-in">
           Ordina i tuoi piatti preferiti
         </h2>
         <p className="hidden lg:block md:text-md md:block lg:text-xl text-violet-600 fade-in">
@@ -21,7 +21,7 @@ const Header = () => {
         </p>
         <a
           href="#explore-menu"
-          className=" py-[0.5vw] px-[2vw] text-xs  lg:block bg-orange-500  rounded-md 
+          className=" py-3 px-6 text-md  lg:block bg-orange-300  rounded-md 
            hover:bg-violet-400 hover:text-white cursor-pointer scrollable-container"
         >
           Vedi il Menu
